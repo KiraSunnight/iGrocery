9418a27a21da78b69ee616ad908e8a37c44ef691
K. Pirozhkov <kirasunnight@gmail.com>
2021-03-01 19:15:01 +0700

    Add necessary functions for goods's class

--- iGroceryQT/iGrocery/entities/good.h
+++ iGroceryQT/iGrocery/entities/good.h
@@ -2,6 +2,7 @@
 #define GOOD_H
 
 #include <QString>
+#include <QDataStream>
 
 class Good
 {
@@ -22,6 +23,28 @@ public:
     void setShelfNumber(int shelfNumber);
     QString providerName() const;
     void setProviderName(const QString &providerName);
+
+    friend bool operator==(const Good &l, const Good &r);
+    friend QDataStream& operator<<(QDataStream &stream, const Good &good);
+    friend QDataStream& operator>>(QDataStream &stream, Good &good);
 };
+
+bool operator==(const Good &l, const Good &r)
+{
+    return l.mName == r.mName && l.mAmountInStorage == r.mAmountInStorage
+            && l.mShelfNumber == r.mShelfNumber && l.mProviderName == r.mShelfNumber;
+}
+
+QDataStream& operator<<(QDataStream &stream, const Good &good)
+{
+    stream << good.mName << good.mAmountInStorage << good.mShelfNumber << good.mProviderName;
+    return stream;
+}
+
+QDataStream& operator>>(QDataStream &stream, Good &good)
+{
+    stream >> good.mName >> good.mAmountInStorage >> good.mShelfNumber >> good.mProviderName;
+    return stream;
+}
 
 #endif // GOOD_H
