372d339b0092956735ed85e00c696619f731efcd
K. Pirozhkov <kirasunnight@gmail.com>
2021-03-01 19:12:00 +0700

    Add necessary functions for user's class

--- iGroceryQT/iGrocery/entities/user.h
+++ iGroceryQT/iGrocery/entities/user.h
@@ -2,6 +2,7 @@
 #define USER_H
 
 #include <QString>
+#include <QDataStream>
 
 enum class UserType {
     WORKER,
@@ -32,6 +33,28 @@ public:
     bool isPasswordCorrect(const QString &password) const;
     UserType userType() const;
     void setUserType(const UserType &userType);
+
+    friend bool operator==(const User &l, const User &r);
+    friend QDataStream& operator<<(QDataStream &stream, const User &usr);
+    friend QDataStream& operator>>(QDataStream &stream, User &usr);
 };
+
+bool operator==(const User &l, const User &r)
+{
+    return l.mName == r.mName && l.mSurname == r.mSurname && l.mPatronymic == r.mPatronymic
+            && l.mLogin == r.mLogin && l.mPassword == r.mPassword && l.mUserType == r.mUserType;
+}
+
+QDataStream& operator<<(QDataStream &stream, const User &usr)
+{
+    stream << usr.mName << usr.mSurname << usr.mPatronymic << usr.mLogin << usr.mPassword << usr.mUserType;
+    return stream;
+}
+
+QDataStream& operator>>(QDataStream &stream, User &usr)
+{
+    stream >> usr.mName >> usr.mSurname >> usr.mPatronymic >> usr.mLogin >> usr.mPassword >> usr.mUserType;
+    return stream;
+}
 
 #endif // USER_H
