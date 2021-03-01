8d4c4e192c6577d6f0eb3968893ffc26c5e8a6ad
K. Pirozhkov <kirasunnight@gmail.com>
2021-03-02 00:22:46 +0700

    Changed goods editing dialog

--- iGroceryQT/iGrocery/gooddialog.cpp
+++ iGroceryQT/iGrocery/gooddialog.cpp
@@ -12,3 +12,46 @@ GoodDialog::~GoodDialog()
 {
     delete ui;
 }
+
+Good *GoodDialog::getGood()
+{
+    return mGood;
+}
+
+void GoodDialog::setGood(Good *good, bool powerUser)
+{
+    mGood = good;
+    ui->baseInfoBox->setEnabled(powerUser);
+    ui->name->setText(mGood->name());
+    ui->provider->setText(mGood->providerName());
+    ui->amountInStorage->setValue(mGood->amountInStorage());
+    ui->shelfNumber->setValue(mGood->shelfNumber());
+}
+
+void GoodDialog::accept()
+{
+    try {
+        QString name = ui->name->text().trimmed();
+        QString provider = ui->provider->text().trimmed();
+        if (name.isEmpty() || provider.isEmpty())
+        {
+            throw std::runtime_error("Проверьте правильность заполнения формы!");
+        }
+
+        int amountInStorage = ui->amountInStorage->value();
+        int shelfNumber = ui->shelfNumber->value();
+        if (mGood == nullptr)
+        {
+            mGood = new Good();
+        }
+        mGood->setName(name);
+        mGood->setProviderName(provider);
+        mGood->setAmountInStorage(amountInStorage);
+        mGood->setShelfNumber(shelfNumber);
+    }  catch (const std::runtime_error &e) {
+        QMessageBox::critical(this, "Ошибка", e.what());
+        return;
+    }
+
+    QDialog::accept();
+}
--- iGroceryQT/iGrocery/gooddialog.h
+++ iGroceryQT/iGrocery/gooddialog.h
@@ -2,6 +2,9 @@
 #define GOODDIALOG_H
 
 #include <QDialog>
+#include <QMessageBox>
+
+#include "./entities/good.h"
 
 namespace Ui {
 class GoodDialog;
@@ -15,8 +18,15 @@ public:
     explicit GoodDialog(QWidget *parent = nullptr);
     ~GoodDialog();
 
+    Good* getGood();
+    void setGood(Good *good, bool powerUser);
+public slots:
+    void accept();
+
 private:
     Ui::GoodDialog *ui;
+
+    Good *mGood = nullptr;
 };
 
 #endif // GOODDIALOG_H
